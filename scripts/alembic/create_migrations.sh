8b8322366ee99bdeae35128fb91487be43f14f67
hush-tukul <hush-tukul@protonmail.com>
2023-09-30 13:07:15 +0200

    Initial commit

--- scripts/alembic/create_migrations.sh
+++ scripts/alembic/create_migrations.sh
@@ -1,3 +1,3 @@
 read -p "Enter name of migration: " message
-export BOT_CONTAINER_NAME=bot_container_name
+export BOT_CONTAINER_NAME=restaurant_bot
 docker exec ${BOT_CONTAINER_NAME} alembic revision --autogenerate -m "$message"
--- scripts/alembic/run_migrations.sh
+++ scripts/alembic/run_migrations.sh
@@ -1,2 +1,2 @@
-export BOT_CONTAINER_NAME=bot_container_name
+export BOT_CONTAINER_NAME=restaurant_bot
 docker exec ${BOT_CONTAINER_NAME} alembic upgrade head
